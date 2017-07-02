bd544b4f2032fdd0db09f15e649fa12ef40c10ff
Loic TRUCHOT <loic.truchot@gmail.com>
2017-07-02 16:25:48 +0700

    chore(develop): add port 3002

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('*', (req, res) => {
 });
 
 // Get port from environment and store in Express.
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3002';
 app.set('port', port);
 
 
